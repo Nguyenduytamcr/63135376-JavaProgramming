a7cc0e31ebc05d75e2a7a5d10990c19a0bff2e44
Nguyễn Duy Tâm <tamnguyen.140103@gmail.com>
2024-01-16 10:49:13 +0700

    Hello World

---
+++ HelloWorld/src/test/ac/xinchao.java
@@ -0,0 +1,11 @@
+package test.ac;
+
+public class xinchao {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+		System.out.println("Tôi mới bắt đầu học JAVA và đang thử nghiệm quản lí mã nguồn GIT");
+	}
+
+}
